e5edad1deb7d7cd975d98bd656ffb2a0fc01fe87
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-14 01:19:21 +0530

    feat: Add Login and Signup Page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,49 @@
-import { Button, Container, Input } from '@mui/joy';
+import { Box, Button, Card, Container, Stack, Typography } from '@mui/joy';
 import { useState } from 'react';
+import FormControl from '../components/FormControl';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  return (
-    <div>
-      <Container>
-        <h2>Login</h2>
-        <Input
-          name='email'
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          fullWidth
-        />
 
-        <Input
-          name='password'
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          fullWidth
-        />
-        <Button></Button>
+  const handleLogin = () => {
+    console.log({ email, password });
+  };
+
+  return (
+    <Box
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      height='100vh'
+    >
+      <Container maxWidth='xs'>
+        <Card variant='outlined' sx={{ p: 3 }}>
+          <Stack spacing={2}>
+            <Typography level='h3' color='primary'>
+              Login
+            </Typography>
+            <FormControl
+              placeholder='Email Address'
+              label='Email'
+              name='email'
+              type='email'
+              value={email}
+              onChange={setEmail}
+            />
+            <FormControl
+              name='password'
+              label='Password'
+              placeholder='Password'
+              type='password'
+              value={password}
+              onChange={setPassword}
+            ></FormControl>
+            <Button onClick={handleLogin}>Login</Button>
+          </Stack>
+        </Card>
       </Container>
-    </div>
+    </Box>
   );
 };
 
