19fd632fa391000aaecec941c876b93c2dc79285
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-14 01:20:32 +0530

    feat: Conditional Render of Header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
+import { CssBaseline, CssVarsProvider } from '@mui/joy';
 const Home = () => {
+  const location = useLocation();
+  const hideHeader =
+    location.pathname === '/login' || location.pathname === '/signup';
+
   return (
-    <div>
-      <Header />
+    <CssVarsProvider>
+      {/* must be used under CssVarsProvider */}
+      <CssBaseline />
+
+      {!hideHeader && <Header />}
       <Outlet />
-    </div>
+      {/* The rest of your application */}
+    </CssVarsProvider>
   );
 };
 
