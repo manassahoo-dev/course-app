4f9ecd671662a62e4a120bd350c428afd002d505
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-13 23:53:42 +0530

    Add Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,32 @@
+import { Button, Container, Input } from '@mui/joy';
+import { useState } from 'react';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div>
+      <Container>
+        <h2>Login</h2>
+        <Input
+          name='email'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+        />
+
+        <Input
+          name='password'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+        />
+        <Button></Button>
+      </Container>
+    </div>
+  );
 };
 
 export default Login;
