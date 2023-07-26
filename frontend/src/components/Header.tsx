3619433992e18de538bbf8e1ad9e18255f588882
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-07-26 23:50:44 +0530

    Code Format

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,21 @@
-import { Box, Typography } from '@mui/joy';
+import { Box, Button, Typography } from '@mui/joy';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', margin: 1 }}>
         <Typography component='div' sx={{ flexGrow: 1 }}>
           Logo
         </Typography>
-        <Link to='/login'>Login</Link>
-        <Link to='/signup'>Signup</Link>
+
+        <Link to='/login'>
+          <Button variant='outlined'>Login</Button>
+        </Link>
+
+        <Link to='/signup'>
+          <Button>Signup</Button>
+        </Link>
       </Box>
     </div>
   );
